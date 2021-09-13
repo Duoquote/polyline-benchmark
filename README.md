8371169ddd8b385d3a0ec0fc287638da62ca0b1e
Duoquote <duoquote@outlook.com>
2021-09-13 21:27:04 +0300

    Added project files.

---
+++ README.md
@@ -0,0 +1,31 @@
+# Benchmarks of Various Implementations of Polyline Decoding in Javascript
+
+## Benchmark:
+###### Ran on Ryzen 5 3600 @ Stock, 32GB ram machine.
+
+```cmd
+> C:\Program Files\nodejs\node.exe index.js
+
+# google-polyline 1000 times
+ok ~8.95 s (8 s + 951970700 ns)
+
+# @googlemaps/polyline-codec 1000 times
+ok ~7.96 s (7 s + 963498400 ns)
+
+# polyline-encoded 1000 times
+ok ~25 s (25 s + 87594900 ns)
+
+# @mapbox/polyline 1000 times
+ok ~8.04 s (8 s + 36240100 ns)
+
+# @mapbox/polyline 1000 times
+ok ~2.45 min (147 s + 810016800 ns)
+
+all benchmarks completed
+ok ~3.28 min (197 s + 849320900 ns)
+```
+
+## TODO:
+[ ] Add Polyline encoding benchmark too.
+[ ] Compare the results and the speed of translation to a specific standard like GeoJSON.
+[ ] Add four stage 1000, 10000, 100000 and 1000000 encodings/decodings benchmark.
---
+++ index.js
@@ -0,0 +1,65 @@
+
+const polylineDec = require("@googlemaps/polyline-codec");
+const googlePolyline = require('google-polyline');
+const polylineEncoded = require('polyline-encoded');
+const mapBoxPolyline = require('@mapbox/polyline');
+const decodePolyline = require('decode-google-map-polyline');
+const fs = require("fs");
+const bench = require('nanobench');
+
+var data = fs.readFileSync("example.csv").toLocaleString();
+data = data.split(/\r\n|\n/);
+data.splice(0, 1);
+data = data.map(line => line.replace(/^"/g, "").replace(/"$/g, ""));
+
+var decoded;
+
+bench('google-polyline 1000 times', function (b) {
+  b.start()
+
+  for (var i = 0; i < 1000; i++) {
+    decoded = data.map(encoded => googlePolyline.decode(encoded));
+  }
+
+  b.end()
+});
+
+bench('@googlemaps/polyline-codec 1000 times', function (b) {
+  b.start()
+
+  for (var i = 0; i < 1000; i++) {
+    decoded = data.map(encoded => polylineDec.decode(encoded));
+  }
+
+  b.end()
+});
+
+bench('polyline-encoded 1000 times', function (b) {
+  b.start()
+
+  for (var i = 0; i < 1000; i++) {
+    decoded = data.map(encoded => polylineEncoded.decode(encoded));
+  }
+
+  b.end()
+});
+
+bench('@mapbox/polyline 1000 times', function (b) {
+  b.start()
+
+  for (var i = 0; i < 1000; i++) {
+    decoded = data.map(encoded => mapBoxPolyline.decode(encoded));
+  }
+
+  b.end()
+});
+
+bench('@mapbox/polyline 1000 times', function (b) {
+  b.start()
+
+  for (var i = 0; i < 1000; i++) {
+    decoded = data.map(encoded => decodePolyline(encoded));
+  }
+
+  b.end()
+});
